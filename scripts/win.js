74bec8ddc0e2bf5f0024f10227a83ff05090b3bb
Manuel Sousa <mlrcbsousa@gmail.com>
2022-12-28 16:57:29 +0100

    Complete all 5 game contracts.

--- scripts/win.js
+++ scripts/win.js
@@ -1,18 +1,44 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1";
+const contractName = "Game5";
+
+// Game 2
+//   let tx = await game.setX(25);
+//   await tx.wait();
+//   tx = await game.setY(25);
+//   await tx.wait();
+//   tx = await game.win();
+
+// Game 3
+//   let tx = await game.win(45);
+
+// Game 4
+//   let tx = await game.win(56);
+
+// Game 5
+//   let tx = await game.giveMeAllowance(10000);
+//   await tx.wait();
+//   tx = await game.mint(10000);
+//   await tx.wait();
+//   tx = await game.win();
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    // let tx = await game.setX(25);
+    // await tx.wait();
+    let tx = await game.giveMeAllowance(10000);
+    await tx.wait();
+    tx = await game.mint(10000);
+    await tx.wait();
+    tx = await game.win();
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
     const receipt = await tx.wait();
-    console.log(receipt);
+    console.log(receipt.events.some(e => e.event === "Winner"));
 }
 
 main()
